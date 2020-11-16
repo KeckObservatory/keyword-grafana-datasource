2d8a7a86a9663d579fad52d54af1ea3877b7d7d0
Paul Richards <prichards@keck.hawaii.edu>
2020-11-16 10:00:21 -1000

    Order data by time, it might not arrive sequentially.

--- pkg/keyword-backend.go
+++ pkg/keyword-backend.go
@@ -217,7 +217,7 @@ func (ds *KeywordDatasource) query(ctx context.Context, query backend.DataQuery,
 	service = pq.QuoteIdentifier(service)
 
 	// Build a SQL query for just counting
-	sql_count := fmt.Sprintf("select count(time) from %s where keyword = $1 and time >= $2 and time <= $3;", service)
+	sql_count := fmt.Sprintf("select count(time) from %s where keyword = $1 and time >= $2 and time <= $3 order by time asc;", service)
 
 	// Run the query once to see how many we are going to get back
 	row := db.QueryRow(sql_count, keyword, from_u, to_u)
@@ -244,7 +244,7 @@ func (ds *KeywordDatasource) query(ctx context.Context, query backend.DataQuery,
 	}
 
 	// Setup and perform the query for the real data set now
-	sql := fmt.Sprintf("select time, binvalue from %s where keyword = $1 and time >= $2 and time <= $3;", service)
+	sql := fmt.Sprintf("select time, binvalue from %s where keyword = $1 and time >= $2 and time <= $3 order by time asc;", service)
 	rows, err := db.Query(sql, keyword, from_u, to_u)
 
 	if err != nil {
