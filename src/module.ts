f4383f68ff3b04e823cfa3e651188e81a2348a34
Paul Richards <prichards@keck.hawaii.edu>
2024-12-13 14:22:11 -1000

    Delete src directory

--- src/module.ts
+++
@@ -1,9 +0,0 @@
-import { DataSourcePlugin } from '@grafana/data';
-import { DataSource } from './DataSource';
-import { ConfigEditor } from './ConfigEditor';
-import { QueryEditor } from './QueryEditor';
-import { KeywordQuery, KeywordDataSourceOptions } from './types';
-
-export const plugin = new DataSourcePlugin<DataSource, KeywordQuery, KeywordDataSourceOptions>(DataSource)
-  .setConfigEditor(ConfigEditor)
-  .setQueryEditor(QueryEditor);
